f2d8db3d8ab3c0e544dee48e8158e2808a93848e
Arian <arian.kulmer@web.de>
2025-12-15 10:00:25 +0100

    marble: Update blobs and firmware from OS2.0.211.0.VMRMIXM
    Change-Id: I2a227f07a66b8fed610ae91d4bcdd281808c0660

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 #
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-10-01
+VENDOR_SECURITY_PATCH := 2025-11-01
 
 # Inherit from xiaomi sm8450-common
 include device/xiaomi/sm8450-common/BoardConfigCommon.mk
